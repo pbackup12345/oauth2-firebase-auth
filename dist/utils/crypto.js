2b1b983a3b919b48d07ca583ef280400d5b4a772
Peter Szemán <p.szeman@gmail.com>
2023-02-04 16:06:43 +0100

    new

--- dist/utils/crypto.js
+++ dist/utils/crypto.js
@@ -14,9 +14,12 @@ Crypto.encrypt = (text) => {
     return `${iv.toString("hex")}:${encrypted.toString("hex")}`;
 };
 Crypto.decrypt = (text) => {
+    console.log(text);
     const divided = text.split(":");
     const iv = Buffer.from(divided.shift(), "hex");
     const encrypted = Buffer.from(divided.join(":"), "hex");
+    console.log(encrypted);
+    console.log(configuration_1.Configuration.instance.crypto_auth_token_secret_key_32);
     const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(configuration_1.Configuration.instance.crypto_auth_token_secret_key_32, "ascii"), iv);
     let decrypted = decipher.update(encrypted);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -20,9 +20,12 @@ export class Crypto {
   };
 
   static decrypt = (text: string): string => {
+    console.log(text)
     const divided = text.split(":");
     const iv = Buffer.from(divided.shift()!, "hex");
     const encrypted = Buffer.from(divided.join(":"), "hex");
+    console.log(encrypted)
+    console.log(Configuration.instance.crypto_auth_token_secret_key_32)
     const decipher = crypto.createDecipheriv(
       "aes-256-cbc",
       Buffer.from(
